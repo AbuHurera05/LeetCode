d6532c9f2abf1a43ec0e30dad1bd14b1896dbe56
AbuHurera05 <junejoabuhurera52@gmail.com>
2025-06-01 22:08:52 +0500

    update

--- LC50_Power.java
+++ LC50_Power.java
@@ -1,16 +1,19 @@
 public class LC50_Power {
    public static void main(String[] args) {
       double x=2.0000;
-      int n=-2;
+      int n=-2147483648;
       double ans=myPow(x,n);
       System.out.println(ans);
    }
    private static double myPow(double x, int n){
       double ans=1;
-      if(n<0){
+      if(n<0 ){
          x=1/x;
          n=n*-1;
       }
+      if(x==1)
+         return ans;
+      
       // double newx= 1/x;
       // int p =Math.abs(n);
       for(int i=0; i<n;i++){
